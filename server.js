08119f04cf52620748d5dc63689570466f86c2cf
Donavan <donavan.aziaka.1@ulaval.ca>
2020-03-23 04:33:42 -0400

    for deployment

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(path.join(__dirname, '/dist')));
-app.use(express.static(path.join(__dirname, 'src/assets')));
+// app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join()));
+app.use(express.static(path.join(__dirname, 'index.html')));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile('./index.html');
 });
 
 app.listen(port);
