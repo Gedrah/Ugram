4f8f566521801bee25a38399681d7be8588401bd
Donavan <donavan.aziaka.1@ulaval.ca>
2020-03-23 20:58:29 -0400

    for deployment

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 // the __dirname is the current directory from where the script is running
+app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(path.join(__dirname, 'src/assets')));
+app.use(express.static(path.join()));
 app.use(express.static(path.join(__dirname, 'index.html')));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-    res.sendFile('./index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port);
+
+
